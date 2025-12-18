dc1a3c63c61bc56d5d90cd0cd88a0eab22ec82d4
aiyms <252431@astanait.edu.kz>
2025-12-19 03:50:32 +0500

    week 1 greeting & intro

--- week 1/src/com/aiym/vetclinic/Main.java
+++ week 1/src/com/aiym/vetclinic/Main.java
@@ -1,7 +1,7 @@
 package com.aiym.vetclinic;
 public class Main {
     public static void main(String[] args){
-        System.out.println("Welcome to the Veterinary Clinic Management System!");
+        System.out.println("Hi! Welcome to the Veterinary Clinic Management System!");
         System.out.println("This system is designed to support the daily work of Veterinary Clinic :)");
         System.out.println("Main features planned for this system include:");
         System.out.println("- Managing pets and their owners");
