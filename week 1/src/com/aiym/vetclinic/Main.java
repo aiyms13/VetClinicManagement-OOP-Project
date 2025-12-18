0db9422452a31af3541fa2e29050e7ee6093d633
aiyms <252431@astanait.edu.kz>
2025-12-19 03:35:11 +0500

    week 1

---
+++ week 1/src/com/aiym/vetclinic/Main.java
@@ -0,0 +1,10 @@
+package com.aiym.vetclinic;
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Veterinary Clinic Management System!");
+        System.out.println("This system is designed to support the daily work of Veterinary Clinic :)");
+        System.out.println("Main features planned for this system include:");
+        System.out.println("- Managing pets and their owners");
+        System.out.println("- Scheduling veterinary appointments");
+    }
+}
